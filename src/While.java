fe53f1c314e4226ff3643b67a673857b5644b641
Raul <raulbaumgarten@hotmail.com>
2025-04-17 15:53:03 -0300

    Atualizando estudo

---
+++ src/While.java
@@ -0,0 +1,23 @@
+public class While {
+    public static void main(String[] args) {
+        int contador = 5;
+
+//        while (contador <= 10) {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//
+//        }
+//
+//        while (contador <= 8) {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//        }
+
+        do {
+            System.out.println("Contador: " + contador);
+            contador++;
+        } while (contador <= 10);
+
+    }
+}
+
